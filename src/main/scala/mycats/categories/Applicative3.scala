0d120425b8a838dd99f68921af24e0815ba017a8
Hermann Hueck <hermann.hueck@mailbox.org>
2018-08-15 19:18:57 +0200

    Applicative improved

--- src/main/scala/mycats/categories/Applicative3.scala
+++ src/main/scala/mycats/categories/Applicative3.scala
@@ -6,25 +6,25 @@ import scala.language.higherKinds
 // in terms of ap and pure. Here we go the other way round:
 // We add pure and implement map2 and ap in terms of map and product.
 //
-trait Applicative2[F[_]] extends Any with Functor[F] { self =>
+trait Applicative3[F[_]] extends Any with Functor[F] { self =>
 
   def pure[A](a: A): F[A]
 
-  def map2[A, B, Z](fa: F[A], fb: F[B])(f: (A, B) => Z): F[Z]
+  override def map[A, B](fa: F[A])(f: A => B): F[B] // already defined in Functor
+
+  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)]
 
   // --- implemented in terms of the above
 
   // map2 in terms of map and product
-  override def map[A, B](fa: F[A])(f: A => B): F[B] =
-    map2(fa, pure(()))((a, _) => f(a))
+  def map2[A, B, Z](fa: F[A], fb: F[B])(f: (A, B) => Z): F[Z] =
+    map(product(fa, fb)) { case (a, b) => f(a, b) }
 
   // ap implemented in terms of map2
-  def ap[A, B](ff: F[A => B])(fa: F[A]): F[B] = map2(ff, fa)((f, a) => f(a))
+  def ap[A, B](ff: F[A => B])(fa: F[A]): F[B] =
+    map2(ff, fa)((f, a) => f(a)) // or: map2(ff, fa)(_(_))
 
   def <*>[A, B](ff: F[A => B])(fa: F[A]): F[B] = ap(ff)(fa) // alias for ap
-
-  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
-    map2(fa, fb)((_, _))
 
   def tuple2[A, B](fa: F[A], fb: F[B]): F[(A, B)] = product(fa, fb)  // alias for product
 
